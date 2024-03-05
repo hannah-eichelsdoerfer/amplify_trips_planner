326dec1f38ea522da4500ee2325f91606fe1068e
hannah-eichelsdoerfer <hannaheichelsdoerfer@gmail.com>
2024-03-05 14:40:06 +1000

    In this module, you added a GraphQL API for trips using Amplify, and configured create, read, update, and delete trips functionality in your app.

--- lib/features/trip/ui/trips_list/trips_list_page.dart
+++ lib/features/trip/ui/trips_list/trips_list_page.dart
@@ -1,13 +1,58 @@
-import 'package:flutter/material.dart';
-import 'package:amplify_trips_planner/common/utils/colors.dart' as constants;
+// import 'package:flutter/material.dart';
+// import 'package:amplify_trips_planner/common/utils/colors.dart' as constants;
 
-class TripsListPage extends StatelessWidget {
+// class TripsListPage extends StatelessWidget {
+//   const TripsListPage({
+//     super.key,
+//   });
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         centerTitle: true,
+//         title: const Text(
+//           'Amplify Trips Planner',
+//         ),
+//         backgroundColor: const Color(constants.primaryColorDark),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: () {},
+//         backgroundColor: const Color(constants.primaryColorDark),
+//         child: const Icon(Icons.add),
+//       ),
+//       body: const Center(
+//         child: Text('Trips List'),
+//       ),
+//     );
+//   }
+// }
+
+import 'package:amplify_trips_planner/common/utils/colors.dart' as constants;
+import 'package:amplify_trips_planner/features/trip/controller/trips_list_controller.dart';
+import 'package:amplify_trips_planner/features/trip/ui/trips_gridview/trips_list_gridview.dart';
+import 'package:amplify_trips_planner/features/trip/ui/trips_list/add_trip_bottomsheet.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class TripsListPage extends ConsumerWidget {
   const TripsListPage({
     super.key,
   });
 
+  Future<void> showAddTripDialog(BuildContext context) =>
+      showModalBottomSheet<void>(
+        isScrollControlled: true,
+        elevation: 5,
+        context: context,
+        builder: (sheetContext) {
+          return const AddTripBottomSheet();
+        },
+      );
+
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final tripsListValue = ref.watch(tripsListControllerProvider);
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
@@ -17,12 +62,14 @@ class TripsListPage extends StatelessWidget {
         backgroundColor: const Color(constants.primaryColorDark),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          showAddTripDialog(context);
+        },
         backgroundColor: const Color(constants.primaryColorDark),
         child: const Icon(Icons.add),
       ),
-      body: const Center(
-        child: Text('Trips List'),
+      body: TripsListGridView(
+        tripsList: tripsListValue,
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -124,8 +124,10 @@
 //   }
 // }
 
+import 'package:amplify_api/amplify_api.dart';
 import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
 import 'package:amplify_flutter/amplify_flutter.dart';
+import 'package:amplify_trips_planner/models/ModelProvider.dart';
 import 'package:amplify_trips_planner/trips_planner_app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -150,6 +152,7 @@ Future<void> main() async {
 Future<void> _configureAmplify() async {
   await Amplify.addPlugins([
     AmplifyAuthCognito(),
+    AmplifyAPI(modelProvider: ModelProvider.instance),
   ]);
   await Amplify.configure(amplifyconfig);
 }
