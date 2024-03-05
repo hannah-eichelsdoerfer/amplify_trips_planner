d875568d9e72f0757350b5f4846c949800406836
hannah-eichelsdoerfer <hannaheichelsdoerfer@gmail.com>
2024-03-05 15:54:20 +1000

    In this module, we implemented the UI to display the user’s past trips. In the next module, we will add the trip’s Activity feature to your app.

--- lib/common/navigation/router/router.dart
+++ lib/common/navigation/router/router.dart
@@ -1,5 +1,7 @@
 import 'package:amplify_trips_planner/common/navigation/router/routes.dart';
 import 'package:amplify_trips_planner/features/trip/ui/edit_trip_page/edit_trip_page.dart';
+import 'package:amplify_trips_planner/features/trip/ui/past_trip_page/past_trip_page.dart';
+import 'package:amplify_trips_planner/features/trip/ui/past_trips/past_trips_list.dart';
 import 'package:amplify_trips_planner/features/trip/ui/trip_page/trip_page.dart';
 import 'package:amplify_trips_planner/features/trip/ui/trips_list/trips_list_page.dart';
 import 'package:amplify_trips_planner/models/ModelProvider.dart';
@@ -28,6 +30,19 @@ final router = GoRouter(
         return EditTripPage(
           trip: state.extra! as Trip,
         );
+      },
+    ),
+    GoRoute(
+      path: '/pasttrips',
+      name: AppRoute.pastTrips.name,
+      builder: (context, state) => const PastTripsList(),
+    ),
+    GoRoute(
+      path: '/pasttrip/:id',
+      name: AppRoute.pastTrip.name,
+      builder: (context, state) {
+        final tripId = state.pathParameters['id']!;
+        return PastTripPage(tripId: tripId);
       },
     ),
   ],
--- lib/features/trip/ui/trips_gridview/trip_gridview_item.dart
+++ lib/features/trip/ui/trips_gridview/trip_gridview_item.dart
@@ -1,17 +1,19 @@
 import 'package:amplify_trips_planner/common/navigation/router/routes.dart';
+import 'package:amplify_trips_planner/common/utils/colors.dart' as constants;
 import 'package:amplify_trips_planner/features/trip/ui/trips_gridview/trip_gridview_item_card.dart';
 import 'package:amplify_trips_planner/models/ModelProvider.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
-//  The App will use this for the trips list grid.
 class TripGridViewItem extends StatelessWidget {
   const TripGridViewItem({
     required this.trip,
+    required this.isPast,
     super.key,
   });
 
   final Trip trip;
+  final bool isPast;
 
   @override
   Widget build(BuildContext context) {
@@ -20,14 +22,21 @@ class TripGridViewItem extends StatelessWidget {
       borderRadius: BorderRadius.circular(15),
       onTap: () {
         context.goNamed(
-          AppRoute.trip.name,
+          isPast ? AppRoute.pastTrip.name : AppRoute.trip.name,
           pathParameters: {'id': trip.id},
           extra: trip,
         );
       },
-      child: TripGridViewItemCard(
-        trip: trip,
-      ),
+      child: isPast
+          ? ColorFiltered(
+              colorFilter: const ColorFilter.matrix(constants.greyoutMatrix),
+              child: TripGridViewItemCard(
+                trip: trip,
+              ),
+            )
+          : TripGridViewItemCard(
+              trip: trip,
+            ),
     );
   }
 }
